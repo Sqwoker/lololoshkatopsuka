e14f60c87868073268d5b9885b83211f0977d73d
Sqwoker <36962385+Sqwoker@users.noreply.github.com>
2018-03-01 19:32:30 +0400

    Add files via upload

---
+++ public_html/steamauth/settings.php
@@ -0,0 +1,6 @@
+<?php
+$steamauth['apikey'] = "10E8849A335083D611B187322A565792"; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = "http://lucky-strikee.ru/"; // The main URL of your website displayed in the login page
+$steamauth['logoutpage'] = ""; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = "/"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+?>
